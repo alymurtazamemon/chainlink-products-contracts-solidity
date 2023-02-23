ef73ceab5d5cf43ac654e4c15f540c0adcaa3015
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-23 12:17:03 +0500

    add hardhat configurations

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,6 +4,29 @@ import "hardhat-deploy";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        localhost: {
+            chainId: 31337,
+        },
+        goerli: {
+            chainId: 5,
+            url: process.env.GOERLI_RPC_URL,
+            accounts: [process.env.PRIVATE_KEY!],
+            saveDeployments: true,
+        },
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
