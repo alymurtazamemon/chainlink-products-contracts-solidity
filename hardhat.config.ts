1f0da5add79beef8edf9b4400a10e30652a90ae7
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-02-23 12:21:41 +0500

    install dotenv and import in hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "hardhat-deploy";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
     solidity: "0.8.17",
@@ -23,9 +24,6 @@ const config: HardhatUserConfig = {
         deployer: {
             default: 0,
         },
-    },
-    etherscan: {
-        apiKey: process.env.ETHERSCAN_API_KEY,
     },
 };
 
