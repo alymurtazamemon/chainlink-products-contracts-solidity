84908e8dda1fa15dd2221ed623f7067c6d913162
alymurtazamemon <alimurtaza-assistant@outlook.com>
2023-03-02 17:41:27 +0500

    create NFTFloorPriceConsumerV3 contract

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -1,6 +1,7 @@
 export interface networkConfigItem {
     ethUsdPriceFeedAddress?: string;
     pORReserveFeedAddress?: string;
+    nftFloorPriceFeedAddress?: string;
 }
 
 export interface networkConfigInfo {
@@ -12,6 +13,7 @@ export const networkConfig: networkConfigInfo = {
     5: {
         ethUsdPriceFeedAddress: "0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e",
         pORReserveFeedAddress: "0x3de1bE9407645533CD0CbeCf88dFE5297E7125e6",
+        nftFloorPriceFeedAddress: "0x5c13b249846540F81c093Bc342b5d963a7518145",
     },
 };
 
